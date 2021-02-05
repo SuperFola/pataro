c068fe25cddc2427318b3f0fa7cd6582aca14597
Alexandre Plateau <lexplt.dev@gmail.com>
2021-02-05 22:03:42 +0100

    removing can walk attribute from the tile, adding an explored attribute

--- include/Pataro/Map/Tile.hpp
+++ include/Pataro/Map/Tile.hpp
@@ -1,15 +1,13 @@
 #ifndef PATARO_MAP_TILE_HPP
 #define PATARO_MAP_TILE_HPP
 
-#include <optional>
-
 namespace pat::map::details
 {
     struct Tile
     {
-        bool can_walk = true;  ///< Can we walk through the tile?
+        bool explored = false;  ///< Has the player seen this tile?
 
-        Tile(std::optional<bool> can_walk = {});
+        Tile();
     };
 }
 
--- src/Pataro/Map/Tile.cpp
+++ src/Pataro/Map/Tile.cpp
@@ -2,9 +2,5 @@
 
 using namespace pat::map::details;
 
-Tile::Tile(std::optional<bool> can_walk_)
-{
-    // if provided, alter the value
-    if (can_walk_)
-        can_walk = can_walk_.value();
-}
+Tile::Tile()
+{}
