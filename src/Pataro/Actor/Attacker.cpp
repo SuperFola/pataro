580cb9827226d4c5a7a240facfc1f381f070bade
Alexandre Plateau <lexplt.dev@gmail.com>
2021-02-08 17:53:29 +0100

    fixing bug where we were 'attacking' dead corpses

--- src/Pataro/Actor/Attacker.cpp
+++ src/Pataro/Actor/Attacker.cpp
@@ -17,10 +17,12 @@ void Attacker::attack(pat::Actor* owner, pat::Actor* target, pat::Engine* engine
     if (Destructible* d = target->destructible(); d != nullptr)
     {
         TCODColor text_color = (owner == engine->get_player()) ? TCODColor::red : TCODColor::lightGrey;
-        if (float dmg = d->take_damage(target, m_power, engine); dmg > 0.f)
+        float dmg = m_power - d->defense();
+        if (dmg > 0.f)
             engine->get_gui()->message(text_color, owner->get_name(), " attacks ", target->get_name(), " for ", dmg, " hit points.");
         else
             engine->get_gui()->message(TCODColor::lightGrey, owner->get_name(), " attacks ", target->get_name(), " but it has no effect!");
+        d->take_damage(target, m_power, engine);
     }
     else
         engine->get_gui()->message(TCODColor::lightGrey, owner->get_name(), " attacks ", target->get_name(), " in vain");
