0f3c5dd2b9fe4173b6ef5615cf04d9b496df4f9d
Alexandre Plateau <lexplt.dev@gmail.com>
2021-03-01 14:12:19 +0100

    better todos

--- src/Pataro/Gui.cpp
+++ src/Pataro/Gui.cpp
@@ -12,7 +12,7 @@ Gui::Gui(unsigned width, unsigned height, const Gui::Proxy_t& proxy) :
 
 void Gui::render(Engine* engine, TCODConsole* dest, int x, int y)
 {
-    // TODO put this elsewhere
+    // TODO put this bar_width elsewhere
     static const int bar_width = 20;
 
     m_con->setDefaultBackground(TCODColor::black);
