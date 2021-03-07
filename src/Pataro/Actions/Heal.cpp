3c15f1424166df6418acc4994516f2cec703a712
Alexandre Plateau <lexplt.dev@gmail.com>
2021-03-07 16:08:12 +0100

    the heal action should talk to the gui

--- src/Pataro/Actions/Heal.cpp
+++ src/Pataro/Actions/Heal.cpp
@@ -10,11 +10,13 @@ HealAction::HealAction(pat::Entity* source, float quantity) :
     m_source(source), m_quantity(quantity)
 {}
 
-pat::ActionResult HealAction::perform([[maybe_unused]] pat::Engine* engine)
+pat::ActionResult HealAction::perform(pat::Engine* engine)
 {
     if (pat::component::Destructible* d = m_source->destructible(); d != nullptr)
     {
-        d->heal(m_quantity);
+        float healed = d->heal(m_quantity);
+        engine->get_gui()->message(TCODColor::lightGreen, m_source->get_name(), " healed ", healed, "HP");
+
         return pat::ActionResult::Success;
     }
     return pat::ActionResult::Fail;
